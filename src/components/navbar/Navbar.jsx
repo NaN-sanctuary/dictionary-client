c85419dc9c9d4147c33bf0d9c155d42830b447e4
nikitabm <nikitabalabuiev@gmail.com>
2023-10-07 13:55:01 +0200

    implement navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,36 @@
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-
+import { FaBeer } from 'react-icons/fa';
 import './NavBar.css';
 
 export default function NavBar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Головна</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Блог</Link>
-                    </li>
-                    <li>
-                        <Link to="/add">Додати слово</Link>
-                    </li>
-                    <li>
-                        <input type="search" id="search" name="search" placeholder="Пошук"></input>
-                    </li>
-                </ul>
+                <div className="nav-container">
+                    <div className="search-root">
+                        <input className="search" type="search" id="search" name="search" placeholder="Пошук"></input>
+                    </div>
+                    <ul className={click ? "menu active" : "menu"}>
+                        <li className="menu-item">
+                            <Link to="/" onClick={handleClick}>Головна</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/blog" onClick={handleClick}>Блог</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/add" onClick={handleClick}>Додати значення</Link>
+                        </li>
+                    </ul>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <span className="icon">
+                            <FaBeer size={24} />{" "}
+                        </span>
+                    </div>
+                </div>
             </nav >
             <Outlet />
         </>
